a0853dfae20da7a4267b1fd9b59a66d6e73b99c9
Gilberto de Amorim <betoacl@hotmail.com>
2023-10-16 22:14:52 -0300

    Entity class modify

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(1, "Paulo", "paulo@paulo", new Date(), 2000.00, obj);
+        System.out.println(seller);
         System.out.println(obj);
     }
 }
