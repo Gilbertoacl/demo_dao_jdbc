524826630d3d3932e8a362fee0466a9fe432337a
Gilberto de Amorim <betoacl@hotmail.com>
2023-10-21 17:29:23 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,7 +11,8 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(1, "Paulo", "paulo@paulo", new Date(), 2000.00, obj);
+        SellerDao sellerDao = new DaoFactory().createSellerDao();
+
         System.out.println(seller);
-        System.out.println(obj);
     }
 }
